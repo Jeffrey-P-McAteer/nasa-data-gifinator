8390321989101b7303cf9380517be2662645f987
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-06-22 14:44:20 -0400

    yay composite gifs!

--- omi-ozone-gif.py
+++ omi-ozone-gif.py
@@ -107,23 +107,61 @@ def main(args=sys.argv):
   print(f'Downloaded {len(downloaded_files)} files, such as {downloaded_files[:2]}')
   print()
 
+  data_images = []
   for data_file in downloaded_files:
     hdf = h5py.File(data_file, 'r')
     #print(f'UNKNOWN {data_file} = {hdf}, keys = {hdf.keys()}')
-    print_recursive_hdf_tree(hdf)
+    #print_recursive_hdf_tree(hdf)
 
     # Ought to be shaped (360, 180)
     RetrievedCOSurfaceMixingRatioDay = hdf_dataset_by_name(hdf, 'RetrievedCOSurfaceMixingRatioDay')
     RetrievedCOSurfaceMixingRatioNight = hdf_dataset_by_name(hdf, 'RetrievedCOSurfaceMixingRatioNight')
 
-    array = RetrievedCOSurfaceMixingRatioNight[:]
-    # Flip the array so 0,0 is the -180,90 degrees
-    array = numpy.rot90(array)
+    # these two arrays use -9999 to indicate "no value", which we use with PIL for a transparency mask
+    NumberofPixelsDay = hdf_dataset_by_name(hdf, 'NumberofPixelsDay')
+    #print(f'NumberofPixelsDay = {NumberofPixelsDay[:]}')
+    NumberofPixelsNight = hdf_dataset_by_name(hdf, 'NumberofPixelsNight')
+    #print(f'NumberofPixelsNight = {NumberofPixelsNight[:]}')
 
-    img = PIL.Image.fromarray(array.astype('uint8'), 'L') # L = 8 bit grayscale, see https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
+    visible_px_measures = NumberofPixelsNight[:]
+    visible_px_measures = numpy.rot90(visible_px_measures)
+
+    img_array = RetrievedCOSurfaceMixingRatioNight[:]
+    # Flip the img_array so 0,0 is the -180,90 degrees
+    img_array = numpy.rot90(img_array)
+
+    visible_px_mask = []
+    for y in range(0, len(visible_px_measures)):
+      visible_px_mask.append([])
+      for x in range(0, len(visible_px_measures[y])):
+        if int(visible_px_measures[y][x]) > -9999:
+          visible_px_mask[y].append(0)
+        else:
+          visible_px_mask[y].append(254)
+
+    measurements_img = PIL.Image.fromarray(img_array.astype('uint8'), 'L') # L = 8 bit grayscale, see https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
+    measurements_img = measurements_img.convert('RGBA')
+
+    transparency = PIL.Image.new('RGBA', (measurements_img.width, measurements_img.height), (0, 0, 0, 255))
+
+    visible_px_img = PIL.Image.fromarray(numpy.array(visible_px_mask).astype('uint8'), 'L')
+
+    final_img = PIL.Image.composite(transparency, measurements_img, visible_px_img)
+
+    data_images.append(final_img)
+
+    print('.', end='', flush=True)
+
     #img.save('yourimage.thumbnail', 'JPEG')
-    img.show()
-    input('Press Enter to continue')
+    #img.show()
+    #input('Press Enter to continue')
+
+  print()
+  # Save to gif
+  print(f'Saving {len(data_images)} frames to out.gif')
+  img = data_images[0]
+  img.save(fp='out.gif', format='GIF', append_images=data_images, save_all=True, transparency=0, duration=200, loop=0)
+
 
 
 
