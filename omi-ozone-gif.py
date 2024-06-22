aef0f9394fbb6966b08a247a29090539f7081881
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-06-22 13:15:25 -0400

    downloads implemented

--- omi-ozone-gif.py
+++ omi-ozone-gif.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import datetime
 
 # Always run in location of this script, even is CWD started out as something different
 os.chdir(
@@ -17,15 +18,61 @@ def main(args=sys.argv):
     print('Asking for credentials b/c EARTHDATA_USERNAME and EARTHDATA_PASSWORD are not set')
     data_fetcher.earthaccess.login()
 
+  dt_now = datetime.datetime.now()
+  now_year = dt_now.year
+  now_month = dt_now.month
+
   #data_fetcher.earthaccess.
-  results = data_fetcher.earthaccess.search_data(
-      short_name='SEA_SURFACE_HEIGHT_ALT_GRIDS_L4_2SATS_5DAY_6THDEG_V_JPL2205',
-      cloud_hosted=True,
-      bounding_box=(-10, 20, 10, 50),
-      temporal=("2018-02", "2019-03"),
-      count=10
+  datasets = data_fetcher.earthaccess.search_datasets(
+      keyword='Pollution Troposphere',
+      cloud_hosted=False,
+      temporal=(f'{now_year-1}-{now_month:02d}', f'{now_year}-{now_month:02d}'),
+      count=100
   )
-  print(f'results = {results}')
+
+  mopitt_dataset = None
+  for i, dataset in enumerate(datasets):
+    #print(f'datasets[{i}] = {dataset}')
+    dataset_native_id = dataset.get('meta', dict()).get('native-id', '')
+
+    print('Dataset', i, '=', dataset_native_id)
+
+    if 'MOPITT' in dataset_native_id and 'gridded daily means' in dataset_native_id:
+      mopitt_dataset = dataset
+
+  if mopitt_dataset is None:
+    print(f'Error: Cannot find MOPITT from {len(datasets)} datasets searched!')
+    return
+
+  print('=' * 24)
+  mopitt_native_id = mopitt_dataset.get('meta', dict()).get('native-id', '')
+  print(f'mopitt_native_id = {mopitt_native_id}')
+  concept_id = mopitt_dataset.get('meta', dict()).get('concept-id', '')
+  print(f'concept_id={concept_id}')
+  short_name = mopitt_dataset.get('umm', dict()).get('ShortName', '')
+  print(f'short_name={short_name}')
+
+  results = data_fetcher.earthaccess.search_data(
+      # concept_id = concept_id,
+      short_name = short_name,
+      cloud_hosted = True,
+      bounding_box = (-180, -90, 180, 90),
+      temporal=(f'{now_year-1}-{now_month:02d}', f'{now_year}-{now_month:02d}'),
+      count=100
+  )
+
+  for i, r in enumerate(results[:5]):
+    print(f'results[{i}] = {results[i]}')
+
+  print('=' * 24)
+
+  data_dir = os.path.join(os.path.abspath('.'), 'data')
+  os.makedirs(data_dir, exist_ok=True)
+  downloaded_files = data_fetcher.earthaccess.download(
+    results,
+    local_path=data_dir,
+  )
+  print(f'downloaded_files = {downloaded_files}')
 
 
 
